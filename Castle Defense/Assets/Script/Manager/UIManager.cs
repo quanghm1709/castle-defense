4dcc42030d470c6d27132e44b80eb794bcff5207
Quang <quanghm@kis-v.com.vn>
2023-09-09 08:46:13 +0700

    Place unit

--- Castle Defense/Assets/Script/Manager/UIManager.cs
+++ Castle Defense/Assets/Script/Manager/UIManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	#region Test_Code
+	[SerializeField] private GameObject selectCharacter;
+	[SerializeField] private GameObject characterSource;
+	
+	public GameObject SelectCharacter { get { return characterSource; } }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void SelectCharacterButton()
+	{
+		selectCharacter = characterSource;
+	}
+
+	private void Update()
+	{
+		if (selectCharacter != null)
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+				pos = new Vector3((int)Mathf.Round(pos.x), (int)Mathf.Round(pos.y+.5f), characterSource.transform.position.z);
+				Instantiate(selectCharacter, pos, Quaternion.identity);
+				//selectCharacter= null;
+			}
+		}
+	}
+	#endregion
 }
